8c444605b4c5f19f8d9df9fa9aaa0e6c2da54897
grv-dev <gauravjaiswal817@gmail.com>
2026-03-17 10:40:35 +0000

    first commit

---
+++ server.js
@@ -0,0 +1,11 @@
+const http = require('http');
+const fs = require('fs');
+const server = http.createServer(function(req, res){
+    fs.readFile('index.html', function(err, data){
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(data);
+        res.end();
+    });
+});
+server.listen(3000);
+    console.log('Server is running on port 3000');
